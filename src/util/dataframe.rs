d9c73c1a1752e144aea4142af0730a26fda675ea
jheryer <jheryer@gmail.com>
2024-04-13 10:13:11 -0500

    created schema modifier and INT_RNG type

--- src/util/dataframe.rs
+++ src/util/dataframe.rs
@@ -1,6 +1,11 @@
 use crate::util::fake;
 use crate::util::schema::Schema;
+use polars::chunked_array::iterator::par;
 use polars::prelude::*;
+use regex::Regex;
+use std::error::Error;
+
+type RangeParseResult = Result<(i32, i32), Box<dyn Error>>;
 
 pub fn create_dataframe(schema: Vec<Schema>, size: usize) -> DataFrame {
     let mut cols = Vec::new();
@@ -15,7 +20,25 @@ pub fn create_dataframe(schema: Vec<Schema>, size: usize) -> DataFrame {
                 element.name.as_str(),
                 build_data_vector(size, fake::fake_int),
             ),
-            "INT_INC" => Series::new(element.name.as_str(), build_incremental_int(size)),
+            "INT_INC" => Series::new(
+                element.name.as_str(),
+                build_incremental_int(size as i32, 0, size.clone() as i32),
+            ),
+            "INT_RNG" => {
+                let (lower, upper) =
+                    match parse_range_string(element.modifier.as_ref().unwrap().as_str()) {
+                        Ok((lower, upper)) => (lower, upper),
+                        Err(e) => {
+                            eprintln!("Error parsing range: {} , using default range", e);
+                            (0, size as i32)
+                        }
+                    };
+
+                Series::new(
+                    element.name.as_str(),
+                    build_incremental_int(size as i32, lower, upper),
+                )
+            }
             "VALUE" => Series::new(
                 element.name.as_str(),
                 build_data_vector(size, fake::value_string),
@@ -112,6 +135,77 @@ fn build_data_vector<T>(size: usize, generator: impl Fn() -> T) -> Vec<T> {
     data
 }
 
-fn build_incremental_int(size: usize) -> Vec<i32> {
-    (0..size as i32).collect::<Vec<i32>>()
+fn build_incremental_int(size: i32, start: i32, end: i32) -> Vec<i32> {
+    let end = if start - end < 0 { start + size } else { end };
+    println!("start: {}, end: {}", start, end);
+    (start..end as i32).collect::<Vec<i32>>()
+}
+
+fn parse_range_string(range_str: &str) -> RangeParseResult {
+    let re = Regex::new(r"\((\d+)-(\d+)\)").unwrap();
+    if let Some(caps) = re.captures(range_str) {
+        let lower: i32 = caps.get(1).unwrap().as_str().parse()?;
+        let upper: i32 = caps.get(2).unwrap().as_str().parse()?;
+        Ok((lower, upper))
+    } else {
+        Err(format!("Error parsing {}", range_str).into())
+    }
+}
+
+mod test {
+    use super::*;
+    use crate::util::schema::Schema;
+
+    #[test]
+    fn test_create_dataframe() {
+        let schema = vec![
+            Schema {
+                name: String::from("col1"),
+                datatype: String::from("INT"),
+                modifier: None,
+            },
+            Schema {
+                name: String::from("col2"),
+                datatype: String::from("STRING"),
+                modifier: None,
+            },
+        ];
+
+        let df = create_dataframe(schema, 10);
+        assert_eq!(df.shape(), (10, 2));
+    }
+
+    #[test]
+    fn test_build_data_vector() {
+        let data = build_data_vector(10, || 1);
+        assert_eq!(data, vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+    }
+
+    #[test]
+    fn test_build_incremental_int() {
+        let data = build_incremental_int(10, 0, 10);
+        assert_eq!(data, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+    #[test]
+    fn test_build_incremental_int_with_negative() {
+        let data = build_incremental_int(10, -10, 10);
+        assert_eq!(data, vec![-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]);
+    }
+
+    #[test]
+    fn test_build_incremental_underun_size() {
+        let data = build_incremental_int(10, 0, 5);
+        assert_eq!(data, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+    #[test]
+    fn test_build_incremental_overrun_size() {
+        let data = build_incremental_int(10, 0, 200);
+        assert_eq!(data, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn test_parse_range_string() {
+        let data = parse_range_string("(0-10)");
+        assert_eq!(data.unwrap(), (0, 10));
+    }
 }
--- src/util/schema.rs
+++ src/util/schema.rs
@@ -1,19 +1,24 @@
 pub struct Schema {
     pub name: String,
     pub datatype: String,
+    pub modifier: Option<String>,
 }
 
 impl Schema {
     pub fn from_string(input: &str) -> Option<Schema> {
         let input = input.replace(" ", "");
-        let parts: Vec<&str> = input.splitn(2, ':').collect();
-        if parts.len() != 2 {
+        let parts: Vec<&str> = input.split(':').collect();
+        if !(parts.len() == 2 || parts.len() == 3) {
             println!("Bad Schema: {:?} is invalid", parts);
             None
         } else {
             Some(Schema {
                 name: parts[0].trim().to_string(),
                 datatype: parts[1].trim().to_string(),
+                modifier: match parts.get(2) {
+                    Some(xyz) => Some(xyz.trim().to_string()),
+                    None => None,
+                },
             })
         }
     }
@@ -33,18 +38,22 @@ pub fn default_schema() -> Vec<Schema> {
         Schema {
             name: String::from("col1"),
             datatype: String::from("VALUE"),
+            modifier: None,
         },
         Schema {
             name: String::from("col2"),
             datatype: String::from("VALUE"),
+            modifier: None,
         },
         Schema {
             name: String::from("col3"),
             datatype: String::from("VALUE"),
+            modifier: None,
         },
         Schema {
             name: String::from("col4"),
             datatype: String::from("VALUE"),
+            modifier: None,
         },
     ]
 }
@@ -52,20 +61,26 @@ pub fn default_schema() -> Vec<Schema> {
 #[cfg(test)]
 mod test {
 
+    use std::ops::Sub;
+
     use super::*;
 
     #[test]
     fn test_happy_path_schema_parser() {
-        let input = "col1:STRING, col2:INT, col3:DATE, ";
+        let input = "col1:STRING, col2:INT, col3:DATE, col4:INT_RNG:(1-  100) ";
         let subject = parse_schema(input);
 
-        assert_eq!(3, subject.len());
+        assert_eq!(4, subject.len());
         assert_eq!("col1", subject.get(0).unwrap().name);
         assert_eq!("STRING", subject.get(0).unwrap().datatype);
         assert_eq!("col2", subject.get(1).unwrap().name);
         assert_eq!("INT", subject.get(1).unwrap().datatype);
         assert_eq!("col3", subject.get(2).unwrap().name);
         assert_eq!("DATE", subject.get(2).unwrap().datatype);
+        assert_eq!("col4", subject.get(3).unwrap().name);
+        assert_eq!("INT_RNG", subject.get(3).unwrap().datatype);
+        let modifier = subject.get(3).unwrap().modifier.as_ref().unwrap();
+        assert_eq!("(1-100)", modifier);
     }
 
     #[test]
